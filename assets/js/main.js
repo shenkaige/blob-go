5df122e16919692ad38fba3e925df1421f92387e
bhat <bhat@encom.eu.org>
2017-12-21 20:36:06 +0800

    Minor font changes.

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,12 @@ function showSnackbar(mesg) {
         x.className = x.className.replace("show", "");
     }, 3000);
 }
+
+function randomColor() {
+    var classes = ["category-green", "category-blue", "category-purple", "category-red"];
+
+    var cates = document.getElementsByClassName('category');
+    Array.prototype.filter.call(cates, function (cate) {
+        cate.classList.add(classes[~~(Math.random() * classes.length)])
+    });
+}
